042cacb8ea3e0e5cb760d0aad59e9aa31247f06e
S P Wright <7489235+sp-wright@users.noreply.github.com>
2020-05-17 18:01:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+$("#map").hide();
+$(".open-map").click(initMap, function () {
+  $("#map").show();
+});
+
+$(".open-map").click(getISS);
+
+function initMap(p) {
+  var iss = p.coords;
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: iss,
+    mapTypeId: "terrain",
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#324f53" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#324f53" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#324f53" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+  });
+  var marker = new google.maps.Marker({
+    position: iss,
+    map: map,
+    icon: `images/iss-s.png`,
+  });
+  console.log("hello");
+}
+
+function getISS() {
+  $.getJSON("http://api.open-notify.org/iss-now.json?callback=?", function (data) {
+    let latitude = Number(data["iss_position"]["latitude"]);
+    let longitude = Number(data["iss_position"]["longitude"]);
+    initMap({ coords: { lat: latitude, lng: longitude } });
+  });
+  setTimeout(getISS, 60000);
+}
